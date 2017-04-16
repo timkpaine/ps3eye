a0e9cb82e63a31d8c60f38369cb484e5b9fd832b
Tim Paine <t.paine154@gmail.com>
2017-04-15 23:42:12 -0400

    .

--- linux/src/ps3eye.cpp
+++ linux/src/ps3eye.cpp
@@ -1,25 +1,52 @@
 #include "opencv2/opencv.hpp"
- 
+#include "context.h"
+#include "frame_function.h"
+
 using namespace cv;
  
-int main(int, char**)
+run_camera(Context& ctx)
 {
     VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
  
     Mat edges;
-    namedWindow("edges",1);
+    namedWindow("Input",1);
+    namedWindow("Output",1);
+
     for(;;)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
+
         // cvtColor(frame, edges, CV_BGR2GRAY);
         // GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
         // Canny(edges, edges, 0, 30, 3);
         // imshow("edges", edges);
-        imshow("edges", frame);
-        if(waitKey(30) >= 0) break;
+        imshow("Input", frame);
+
+        int size = 640*480*3;
+        uint8_t* destimg = 0;
+        if(destimg==0){
+            destimg = new uint8_t[size];
+        }
+        for(int row; row<480; row++){
+            for(int col; col<640; col++){
+                Vec3f pixel = image.at<Vec3f>(row, col);
+                int r = pixel[2];
+                int g = pixel[1];
+                int b = pixel[0];
+                destimg[row*col + col] = r;
+                destimg[row*col + col+1] = g; 
+                destimg[row*col + col+2] = b; 
+            }
+        }
+
+        frame_function(context, destimg);
+
+        Mat m = Mat(480, 640, CV_8UC3, destimg); 
+        imshow("Output", m);
+        waitKey(1);
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
