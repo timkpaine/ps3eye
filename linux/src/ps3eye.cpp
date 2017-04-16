bb1dd096edfc274c89b5cc01098152471a583665
Tim Paine <t.paine154@gmail.com>
2017-04-16 21:12:44 +0000

    .

--- linux/src/ps3eye.cpp
+++ linux/src/ps3eye.cpp
@@ -6,29 +6,6 @@
 using namespace cv;
 using namespace std;
 
-string type2str(int type) {
-  string r;
-
-  uchar depth = type & CV_MAT_DEPTH_MASK;
-  uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-  switch ( depth ) {
-    case CV_8U:  r = "8U"; break;
-    case CV_8S:  r = "8S"; break;
-    case CV_16U: r = "16U"; break;
-    case CV_16S: r = "16S"; break;
-    case CV_32S: r = "32S"; break;
-    case CV_32F: r = "32F"; break;
-    case CV_64F: r = "64F"; break;
-    default:     r = "User"; break;
-  }
-
-  r += "C";
-  r += (chans+'0');
-
-  return r;
-}
-
 int run_camera(Context& context)
 {
     VideoCapture cap(0); // open the default camera
@@ -45,20 +22,15 @@ int run_camera(Context& context)
     for(;;)
     {
         cap >> frame; // get a new frame from camera
-        // cv::cvtColor(frame_raw, frame, CV_BGR2GRAY);
+
         imshow("Input", frame);
-        string ty =  type2str( frame.type() );
-
+        
         int size = context.height*context.width*3;
-        vector<uint8_t> *destimg = 0;
-        if(destimg==0){
-            destimg = new vector<uint8_t>(size);
-        }
-        memcpy(destimg, (uint8_t*) frame.data,size);
 
-        frame_function(context, *destimg);
+        vector<uint8_t> destimg((uint8_t*) frame.data, (uint8_t*)frame.data+size);
+        frame_function(context, destimg);
 
-        Mat m(480, 640, CV_8UC3, destimg); 
+        Mat m(480, 640, CV_8UC3, (uint8_t*) &destimg[0]); 
         imshow("Output", m);
         waitKey(1);
     }
