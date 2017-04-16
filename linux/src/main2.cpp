5e3be27a341b4f2b76bd0ffe18cadb7a3b6e2dd1
Tim Paine <t.paine154@gmail.com>
2017-04-15 22:18:01 -0400

    .

--- linux/src/main2.cpp
+++ linux/src/main2.cpp
@@ -1,27 +1,53 @@
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/core/core.hpp>
+#include <stdio.h>
+#include <time.h>
 
-using namespace cv;
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
 
+int main(int argc, char *argv[]) {
 
-int main()
-{
-    VideoCapture cap(0);
-    if(!cap.isOpened())
-    {
-        return -1;
+  const int cameraIndex = 0;
+  const bool isColor = true;
+  const int w = 320;
+  const int h = 240;
+  const double captureFPS = 187.0;
+  const double writerFPS = 60.0;
+  // With MJPG encoding, OpenCV requires the AVI extension.
+  const char filename[] = "SlowMo.avi";
+  const int fourcc = cv::VideoWriter::fourcc('M','J','P','G');
+  const unsigned int numFrames = 3750;
+
+  cv::Mat mat;
+
+  // Initialize and configure the video capture.
+  cv::VideoCapture capture(cameraIndex);
+  if (!isColor) {
+    capture.set(cv::CAP_PROP_MODE, cv::CAP_MODE_GRAY);
+  }
+  capture.set(cv::CAP_PROP_FRAME_WIDTH, w);
+  capture.set(cv::CAP_PROP_FRAME_HEIGHT, h);
+  capture.set(cv::CAP_PROP_FPS, captureFPS);
+
+  // Initialize the video writer.
+  cv::VideoWriter writer(
+      filename, fourcc, writerFPS, cv::Size(w, h), isColor);
+
+  // Get the start time.
+  clock_t startTicks = clock();
+
+  // Capture frames and write them to the video file.
+  for (unsigned int i = 0; i < numFrames;) {
+    if (capture.read(mat)) {
+      writer.write(mat);
+      i++;
     }
-    Mat frame;
-    Mat edges;
+  }
 
-    bool stop = false;
-    while(!stop)
-    {
-        cap>>frame;
-        imshow("video",frame);
-        if(waitKey(10) >=0)
-            stop = true;
-    }
-    return 0;
+  // Get the end time.
+  clock_t endTicks = clock();
+
+  // Calculate and print the actual frame rate.
+  double actualFPS = numFrames * CLOCKS_PER_SEC /
+      (double)(endTicks - startTicks);
+  printf("FPS: %.1f\n", actualFPS);
 }
