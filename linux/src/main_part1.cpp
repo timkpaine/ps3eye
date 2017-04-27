289f46e09a279552a59729594c329544fc53b6d5
Tim Paine <t.paine154@gmail.com>
2017-04-26 23:18:30 -0400

    .

--- linux/src/main_part1.cpp
+++ linux/src/main_part1.cpp
@@ -10,10 +10,14 @@ using namespace std;
 int main(){
     vector<int> vector_from1 = {
         0,0,0, 1,1,1, 0,0,0, 0,0,0,
-        0,0,0, 1,1,1, 0,0,0, 1,1,1,
+        0,0,0, 1,1,1, 0,0,0, 0,0,0,
     };
 
     vector<int> vector_from2 = {
+        0,0,0, 1,1,1, 0,0,0, 0,0,0,
+        0,0,0, 1,1,1, 0,0,0, 1,1,1,
+    };
+    vector<uint8_t> vector_from3 = {
         0,0,0, 1,1,1, 0,0,0, 0,0,0,
         0,0,0, 1,1,1, 0,0,0, 1,1,1,
     };
@@ -21,7 +25,7 @@ int main(){
     // TODO Practice Templates
     // CameraImage<int> img1(vector_from1, 4, 2);
     // CameraImage<int> img2(vector_from2, 4, 2);
-    // CameraImage<uint8_t> img3(vector_from2, 4, 2);
+    // CameraImage<uint8_t> img3(vector_from3, 4, 2);
 
     // TODO practice operator overloading
     // cout << img1 << endl;
